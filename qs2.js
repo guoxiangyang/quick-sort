9a030b93a0f79e615b47d20fcd35d8750a75da57
guoxiangyang <guoxiangyang@gmail.com>
2016-10-07 17:45:32 +0800

    update

--- qs2.js
+++ qs2.js
@@ -8,12 +8,6 @@ function qs(a) {
     return a.length ? qs(l).concat(k, qs(r)) : [];
 }
 
-function quick_sort(a) {
-    return qs(a, a[0], 0, a.length - 1, 0);
-}
-
-
-
 var arr = [200, 3, 54, 1234, 23, 2354, 34125, 1, 2345, 12, 4325, 123, 12345, 123];
 console.log("input  = ", arr);
-console.log("output = ", quick_sort(arr));
+console.log("output = ", qs(arr));
