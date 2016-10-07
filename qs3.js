e81e87cb5c60f29bf464c6ba89a74840e91eaa12
guoxiangyang <guoxiangyang@gmail.com>
2016-10-07 21:09:11 +0800

    update

--- qs3.js
+++ qs3.js
@@ -5,6 +5,6 @@ function qs(a) {
     return a.length ? qs(a.filter(e => {(e == a[0] ? k : (e > a[0] ? r : [])).push(e); return (e < a[0])})).concat(k, qs(r)) : [];
 }
 
-var arr = [200, 3, 54, 1234, 23, 2354, 34125, 1, 2345, 12, 4325, 123, 12345, 123];
+var arr = [200, 3, 54, 1234, 23, 2354, 3, 1, 3, 3, 34125, 1, 2345, 12, 123, 12345, 123];
 console.log("input  = ", arr);
 console.log("output = ", qs(arr));
